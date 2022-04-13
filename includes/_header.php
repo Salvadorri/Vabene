cb5f76ea1536628938eebd05257849452ab5419e
Rodrigo Salvadori <rodrigo.ssalvadori@gmail.com>
2022-04-13 09:04:37 -0300

    as

--- includes/_header.php
+++ includes/_header.php
@@ -3,10 +3,8 @@
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="empresa.php">Empresa</a></li>
             <li><a href="produtos.php">Produtos</a></li>
             <li><a href="contato.php">Contato</a></li>
-            <li><a href="noticia.php">Notícias</a></li>
             </form>
         </ul>
     </nav>
